60a58b76800543034bc4401329b4747619940c8a
Aditya Mathur <aditya.2024@Adityas-MacBook-Pro-48.local>
2025-12-02 12:12:59 +0530

    final changes

--- backend/setup-db.sql
+++
@@ -1,8 +0,0 @@
--- Create the focusly database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS focusly;
-
--- Use the focusly database
-USE focusly;
-
--- Show all tables (for verification)
-SHOW TABLES;
